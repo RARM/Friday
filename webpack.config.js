4596600f4c91b687b218108f185a1da746a10b25
Rodolfo Andrés Rivas Matta <rodolfo.andres.pro@gmail.com>
2019-03-13 21:19:20 -0400

    Adding css loaders, html plugin and clean webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const softwareVersion = '0.1.0';
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, ('dist/friday-' + softwareVersion + '.browser/')),
+        // File name: (module)-(version).(platform).[min].js
+        filename: 'friday.js'
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+          }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+          template: 'src/resources/html/index.html',
+          inject: 'head'
+        })
+    ],
+
+    mode: 'development'
+
+};
